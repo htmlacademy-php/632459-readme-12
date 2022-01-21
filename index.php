17282388c241ecc8a079e1ed38c2f9d33dbf0523
Алина <alinascarlett@mail.ru>
2022-01-21 16:11:49 +0300

    добавляет условие показа имени пользователя

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once 'helpers.php';
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Алина Глинская'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -42,6 +42,7 @@ $user_name = ''; // укажите здесь ваше имя
         </form>
         <div class="header__nav-wrapper">
             <!-- здесь должен быть PHP код, который показывает следующий тег по условию -->
+            <?php if ($is_auth == 1): ?>
             <nav class="header__nav">
                 <ul class="header__my-nav">
                     <li class="header__my-page header__my-page--popular">
@@ -69,6 +70,7 @@ $user_name = ''; // укажите здесь ваше имя
                             </div>
                             <div class="header__profile-name">
                                 <span>
+                                    <?=$user_name; ?>
                                     <!--здесь должно быть имя пользователя-->
                                 </span>
                                 <svg class="header__link-arrow" width="10" height="6">
@@ -111,6 +113,7 @@ $user_name = ''; // укажите здесь ваше имя
                     </li>
                 </ul>
             </nav>
+            <?php endif; ?>
         </div>
     </div>
 </header>
