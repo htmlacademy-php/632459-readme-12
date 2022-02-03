eb294f4a8befa68be0f4f9af52bb2dfc178c768d
Алина <alinascarlett@mail.ru>
2022-02-03 15:38:30 +0300

    исправляет разделитель

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $popular_posts = [
     ]
 ];
 
-function utf8_wordwrap($string, $width, $break="\n")
+function utf8_wordwrap($string, $width, $break="#!#!")
 {
     $pattern = '/(?=\s)(.{1,'.$width.'})(?:\s|$)/uS';
     $replace = '$1'.$break;
@@ -52,7 +52,7 @@ function utf8_wordwrap($string, $width, $break="\n")
 function clip_post_text(string $string, int $limit = 300): string
 {
 	if (mb_strlen($string) > $limit) {
-		$cutted_array = explode("\n", utf8_wordwrap($string, $limit, "\n"));
+		$cutted_array = explode("#!#!", utf8_wordwrap($string, $limit, "#!#!"));
 		return "<p>" . $cutted_array[0] . "..." . "</p>" . "<a class=\"post-text__more-link\" href=\"#\">Читать далее</a>";
 	}
 	return "<p>" . $string . "</p>";
