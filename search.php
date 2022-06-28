9e92a45b83b75a495d7e72632df695b6d25e44c8
Alina <alinascarlett@mail.ru>
2022-06-28 19:47:18 +0300

    [fix] search.php: исправляет условия

--- search.php
+++ search.php
@@ -16,57 +16,68 @@
 
     $search_text = trim($search_query);
 
-    if (!empty($search_text)) {
-        if (substr($search_text, 0, 1) === "#") {
-            $hashtag = substr($search_text, 1, strlen($search_text) - 1);
+    if (empty($search_text)) {
+        $page_content = include_template('no-results.php', [
+            'search_text' => $search_text,
+        ]);
+        
+        $layout_content = include_template('layout.php', [
+            'content'   => $page_content,
+            'title'     => $page_titles['search_results']
+        ]);
 
-            $sql_posts = 'SELECT posts.*, login, avatar_path, class, type FROM posts '
-            . 'JOIN users u ON user_id = u.id '
-            . 'JOIN content_types ct ON content_type = ct.id '
-            . 'JOIN post_tags pt ON posts.id=pt.post_id '
-            . 'JOIN hashtags h ON pt.hashtag_id=h.id '
-            . 'WHERE hashtag_name = ?';
-
-            $result = form_sql_request($con, $sql_posts, [$hashtag]);
-            $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        } else {
-            $sql_posts = 'SELECT posts.*, login, avatar_path, class, type FROM posts '
-            . 'JOIN users u ON user_id = u.id '
-            . 'JOIN content_types ct ON content_type = ct.id '
-            . 'WHERE MATCH(title,text) AGAINST(?)';
-
-            $result = form_sql_request($con, $sql_posts, [$search_text]);
-            $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        }
-
-        $post_comments = [];
-        $post_likes = [];
-
-        foreach ($posts as $post) {
-            $sql_comments = 'SELECT COUNT(id) AS total FROM comments '
-            . 'WHERE post_id = '. $post['id'];
-            $result = form_sql_request($con, $sql_comments, []);
-            $comments = mysqli_fetch_all($result, MYSQLI_ASSOC);
-            array_push($post_comments, $comments[0]['total']);
-
-            $sql_likes = 'SELECT COUNT(id) AS total FROM likes '
-            . 'WHERE like_post_id = '. $post['id'];
-            $result = form_sql_request($con, $sql_likes, []);
-            $likes = mysqli_fetch_all($result, MYSQLI_ASSOC);
-            array_push($post_likes, $likes[0]['total']);
-        }
+        print($layout_content);
+        return;
     }
+
+    $sql_posts = 'SELECT posts.*, login, avatar_path, class, type FROM posts '
+        . 'JOIN users u ON user_id = u.id '
+        . 'JOIN content_types ct ON content_type = ct.id '
+        . 'WHERE MATCH(title,text) AGAINST(?)';
+
+        $result = form_sql_request($con, $sql_posts, [$search_text]);
+
+    if (substr($search_text, 0, 1) === "#") {
+        $hashtag = substr($search_text, 1, strlen($search_text) - 1);
+
+        $sql_posts = 'SELECT posts.*, login, avatar_path, class, type FROM posts '
+        . 'JOIN users u ON user_id = u.id '
+        . 'JOIN content_types ct ON content_type = ct.id '
+        . 'JOIN post_tags pt ON posts.id=pt.post_id '
+        . 'JOIN hashtags h ON pt.hashtag_id=h.id '
+        . 'WHERE hashtag_name = ?';
+
+        $result = form_sql_request($con, $sql_posts, [$hashtag]);
+    }
+
+    $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $post_comments = [];
+    $post_likes = [];
+
+    foreach ($posts as $post) {
+        $sql_comments = 'SELECT COUNT(id) AS total FROM comments '
+        . 'WHERE post_id = '. $post['id'];
+        $result = form_sql_request($con, $sql_comments, []);
+        $comments = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        array_push($post_comments, $comments[0]['total']);
+
+        $sql_likes = 'SELECT COUNT(id) AS total FROM likes '
+        . 'WHERE like_post_id = '. $post['id'];
+        $result = form_sql_request($con, $sql_likes, []);
+        $likes = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        array_push($post_likes, $likes[0]['total']);
+    }
+
+    $page_content = include_template('search-results.php', [
+        'search_text' => $search_text,
+        'posts' => $posts,
+        'post_comments' => $post_comments,
+        'post_likes' => $post_likes
+    ]);
 
     if (empty($posts)) {
         $page_content = include_template('no-results.php', [
             'search_text' => $search_text,
-        ]);
-    } else {
-        $page_content = include_template('search-results.php', [
-            'search_text' => $search_text,
-            'posts' => $posts,
-            'post_comments' => $post_comments,
-            'post_likes' => $post_likes
         ]);
     }
 
