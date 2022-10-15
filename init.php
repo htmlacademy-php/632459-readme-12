6eec240752e0f6cceb53e5df81e9f36835fa4ded
Alina <alinascarlett@mail.ru>
2022-10-15 21:57:56 +0300

    fix: удаляет режим отображения ошибок из init

--- init.php
+++ init.php
@@ -2,11 +2,6 @@
 
 date_default_timezone_set('Europe/Moscow');
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-ini_set('error_log', __DIR__.'/php-errors.log');
-
 require_once 'config/db.php';
 
 $con = mysqli_connect(
